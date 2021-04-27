a2d13d6f7aa2743fa8c12113772e0a22e1722c13
ejezie <shady07chinex@gmail.com>
2021-04-27 22:24:20 +0100

    creating model and database data

---
+++ backend/data/Product.js
@@ -0,0 +1,68 @@
+const products = [
+  {
+    name: "Samsung Galaxy A51 6.5",
+    description:
+      "With a 6.5 FHD+ widescreen display powered by Super AMOLED technology, the Samsung Galaxy A51 A515F Dual-SIM 128GB Smartphone is a great choice for surfing the web, streaming video, playing games, capturing and viewing photos, and more. With its Infinity-O display and minimized bezels, this screen essentially stretches edge to edge to give you a true widescreen experience.",
+    price: 137990,
+    imageUrl:
+      "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/42/076454/1.jpg?6595",
+  },
+  {
+    name: "Nokia 3.4",
+    description:
+      "Upgrade to more phone and more power with Nokia 3.4. Maximize your streaming, binging and work on a 6.39” HD+ punch hole display that gives you even more viewing room. Don’t sweat running out of juice with 2-day battery life. 1Get the perfect shot with powerful triple camera AI imaging, Portrait mode, Night mode and an ultra-wide lens. Nokia 3.4 gives you style and durability all in one, plus easy access with fingerprint unlock. Not to mention Nokia’s Android One promise – keeping you ahead of the curve with years of security updates and software upgrades. Which means you’ll get more out of your phone, for longer.",
+    price: 59590,
+    imageUrl:
+      "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/37/835317/1.jpg?0847",
+  },
+  {
+    name: "Nokia 5.4",
+    description:
+      "Software is one area that is hard to find fault with on Nokia smartphones. It's stock Android 11 with just Google suite of apps, a support app from HMD Global, and a Cinema editor extension in the Google Photos app for colour grading H-Log videos. The Nokia 5.4 has a 4,000mAh battery, enough to get me through one full day of heavy use, and even the next day if the usage is frugal.",
+    price: 95000,
+    imageUrl:
+      "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/43/108127/1.jpg?9872",
+  },
+  {
+    name: "Tecno Spark 5 Pro",
+    description:
+      "TECNO Spark 5 Pro joins the Spark 5 series family of smartphones in 2020, with a big 5000mAh battery, a big 6.6-inch display, rear-mounted fingerprint scanner, octa-core processor from Mediatek, 4GB of RAM and 128GB of storage memory.",
+    price: 66500,
+    imageUrl:
+      "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/48/696596/1.jpg?7127",
+  },
+  {
+    name: "Huawei Nova 7i",
+    description:
+      "Huawei Nova 7i smartphone comes with a 6.4-inch touchscreen display with a resolution of 1080x2310 pixels at a pixel density of 398 pixels per inch (ppi). Huawei Nova 7i is powered by an octa-core HiSilicon Kirin 810 processor that features 2 cores clocked at 2.27GHz and 6 cores clocked at 1.88GHz. It comes with 8GB of RAM. The Huawei Nova 7i runs Android 10 and is powered by a 4200mAh battery.",
+    price: 129000,
+    imageUrl:
+      "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/59/990917/1.jpg?8328",
+  },
+  {
+    name: "XIAOMI Redmi Note 9",
+    description:
+      "The device is equipped with sensors such as Fingerprint (rear-mounted), accelerometer, gyro, proximity, and compass. The smartphone features Infrared port, USB 2.0 Type C, Radio with recording, and Bluetooth 5.0. The smartphone is fueled by a Non-removable Li-Po 5020 mAh battery + Fast charging 18W + Reverse charging 9W.",
+    price: 84000,
+    imageUrl:
+      "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/56/840086/1.jpg?4635",
+  },
+  {
+    name: "QUBO BIG1-6.26",
+    description:
+      "No need to carried chargers around at all time. Super lasting powered by 4500mAh battery which lets you be more energetic in business, games,videos and reading.  Under test,calling time up to 21 hours, music playback 60 hours and videos playing 8 hours. Big built-in memory let you love more. Qubo BIG1 comes with 16GB of Rom which make you have more room to do more things you like, such games, videos,books and pictures. ",
+    price: 137990,
+    imageUrl:
+      "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/23/245656/1.jpg?9013",
+  },
+  {
+    name: "Infinix Smart 5 (X657)",
+    description:
+      "5000mAh for long-lasting funWith the large 5000 mAh battery and intelligent power-saving technology, you can enjoy your entertainment all day long without power worries. Big battery, Long-lasting battery,Super charge. Waterdrop sunlight display,more view for what you do.A wide 6.6”HD+ beautiful, vibrant screen provides a clear and broader view for a better mobile experience.",
+    price: 47499,
+    imageUrl:
+      "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/89/818846/1.jpg?2703",
+  },
+];
+
+modules.export = products;
