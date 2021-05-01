6ec39360377d8456bc4ca0c06c3e46066f4b6e10
ejezie <shady07chinex@gmail.com>
2021-05-01 20:31:51 +0100

    styling homescreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import "./HomeScreen.css";
 import { Link } from "react-router-dom";
 import smp from "../images/smp.jpg";
+import phone from "../images/phone.jpg";
+import samsung from "../images/samsung.png";
+import nokia from "../images/nokia.png";
+import mi from "../images/mi.png";
+import huawei from "../images/huawei.png";
+import tecno from "../images/tecno.png";
+import qubo from "../images/qubo.png";
 
 function HomeScreen() {
   return (
@@ -15,12 +22,20 @@ function HomeScreen() {
           Doloremque ipsam vero asperiores, quae pariatur, itaque atque esse
           assumenda minus dolorem quia.
         </p>
+        <div className="logos">
+          <img src={samsung} alt="samsung" />
+          <img src={nokia} alt="nokia" />
+          <img src={mi} alt="mi" />
+          <img src={huawei} alt="huawei" />
+          <img src={tecno} alt="tecno" />
+          <img src={qubo} alt="qubo" />
+        </div>
+
         <Link to="/products">
           <button>View Products</button>
         </Link>
       </div>
-      <div className="img__back"></div>
-      <img src={smp} alt="picture" />
+      <img src={phone} alt="picture" />
     </div>
   );
 }
