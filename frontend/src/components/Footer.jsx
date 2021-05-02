c1298540b71dd440e845fdf69941eb395b20c255
ejezie <shady07chinex@gmail.com>
2021-05-02 07:52:55 +0100

    styling products screen

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="navbar__log">
+        <h2>
+          ShopMobile<i class="fad fa-mobile"></i>
+        </h2>
+      </div>
+      <div className="foot__icons">
+        <p>contact us</p>
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-facebook-square"></i>
+        <i class="fab fa-youtube"></i>
+        <i class="fad fa-mobile"></i>
+      </div>
+      <div className="search">
+        <input type="text" placeholder="search product" />
+        <i class="fas fa-search"></i>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- frontend/src/screens/ScreenProducts.jsx
+++ frontend/src/screens/ScreenProducts.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./ScreenProducts.css";
 import Product from "../components/Product";
+import Footer from "../components/Footer"
 
 function ScreenProducts() {
   return (
     <div className="screenproducts">
       <div className="head__search">
         <h2>ANDRIOD SMART PHONES</h2>
+        <div className="search">
+          <input type="text" placeholder="search product"/>
+          <i class="fas fa-search"></i>
+        </div>
       </div>
       <div className="products">
         <Product />
@@ -18,6 +23,7 @@ function ScreenProducts() {
         <Product />
         <Product />
       </div>
+      <Footer/>
     </div>
   );
 }
