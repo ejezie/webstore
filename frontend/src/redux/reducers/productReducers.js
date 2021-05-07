a974374132040e74ef16636bb3acdcb2a07867ca
ejezie <shady07chinex@gmail.com>
2021-05-07 09:56:11 +0100

    produts reducer setup

--- frontend/src/redux/reducers/productReducers.js
+++ frontend/src/redux/reducers/productReducers.js
@@ -1,5 +1,47 @@
 import * as actionTypes from "../constants/productConstants";
 
-export const productsReducers = (state = {products=[]}, action) => {
-    
-}
+export const getProductsReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case actionTypes.GET_PRODUCTS_REQUEST:
+      return {
+        loading: true,
+      };
+    case actionTypes.GET_PRODUCTS_SUCCESS:
+      return {
+        loading: false,
+        payload: action.payload,
+      };
+    case actionTypes.GET_PRODUCTS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    default:
+        return state;
+  }
+};
+
+export const getProductDetailsReducer = (state = { products: {} }, action) => {
+  switch (action.type) {
+    case actionTypes.GET_PRODUCT_DETAIL_REQUEST:
+      return {
+        loading: true,
+      };
+    case actionTypes.GET_PRODUCT_DETAIL_SUCCESS:
+      return {
+        loading: false,
+        payload: action.payload,
+      };
+    case actionTypes.GET_PRODUCT_DETAIL_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case actionTypes.GET_PRODUCT_DETAIL_RESET:
+      return {
+        product: {},
+      };
+    default:
+      return state;
+  }
+};
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,13 +1,23 @@
-import {createStore, combineReducers, applyMiddleware} from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import {composeWithDevTools} from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { cartReducer } from "./reducers/cartReducers";
+import {
+  getProductsReducer,
+  getProductDetailsReducer,
+} from "./reducers/productReducers";
 
 const reducer = combineReducers({
-
-})
+  cart: cartReducer,
+  getProducts: getProductsReducer,
+  getProductsDetails: getProductDetailsReducer,
+});
 
 const middleware = [thunk];
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
