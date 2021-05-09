115434bdc291ec20c58b6e04f759a1142498c130
ejezie <shady07chinex@gmail.com>
2021-05-09 23:13:12 +0100

    installing react spinner and displaying products

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Product.css";
 
-function Product() {
+
+function Product({imageUrl, name, price, description, productId}) {
   return (
     <div className="product">
       <img
-        src="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/43/108127/1.jpg?9872"
-        alt="phone"
+        src={imageUrl}
+        alt={name}
       />
       <div className="product__info">
-        <p className="info__name">Phone</p>
+        <p className="info__name">{name}</p>
         <p className="info__description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
-          nobis impedit officiis eaque voluptatem nihil eveniet.
+          {description.substring(0,100)}...
         </p>
-        <p className="info__price">$200.00</p>
-        <Link to={`product/${1111}`} className="info__button">
+        <p className="info__price">${price}</p>
+        <Link to={`product/${productId}`} className="info__button">
           View
         </Link>
       </div>
--- frontend/src/screens/ScreenProducts.jsx
+++ frontend/src/screens/ScreenProducts.jsx
@@ -5,16 +5,33 @@ import Product from "../components/Product";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getProducts as listProducts } from "../redux/actions/productActions";
+import ClipLoader from "react-spinners/PuffLoader";
 
 function ScreenProducts() {
   const dispatch = useDispatch();
 
   const getProducts = useSelector((state) => state.getProducts);
-  const {products, loading, error,} = getProducts;
+  const { products, loading, error } = getProducts;
 
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
+
+  const override = `
+   margin: 10em 0 0 42em ;
+   @media (max-width:1000px) {
+    margin: 15em 0 0 24em ;
+  };
+   @media (max-width:800px) {
+    margin: 12em 0 0 17em ;
+  };
+   @media (max-width:565px) {
+    margin: 10em 0 0 10em ;
+  };
+   @media (max-width:395px) {
+    margin: 5em 0 0 5em ;
+  };
+  `
 
   return (
     <div className="screenproducts">
@@ -28,13 +45,20 @@ function ScreenProducts() {
         </div>
         <div className="products">
           {loading ? (
-            <h2>
-              <i class="fad fa-spinner"></i>
-            </h2>
+             <ClipLoader loading={loading} color={"#4A90E2"} css={override} size={150} />
           ) : error ? (
             <h2>{error}</h2>
           ) : (
-            products.map(product => <Product />)
+            products.map((product) => (
+              <Product
+                key={product._id}
+                productId={product._id}
+                price={product.price}
+                imageUrl={product.imageUrl}
+                description={product.description}
+                name={product.name}
+              />
+            ))
           )}
         </div>
       </div>
